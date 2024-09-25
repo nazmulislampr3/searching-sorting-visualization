4a60accd06d6ff870e11213d1e95040bc8a855ab
Md Nazmul Islam <nazmulislampr3@gmail.com>
2024-09-25 18:58:14 +0600

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet, redirect, useLocation } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import Menu from "./components/Menu";
 
 const App = () => {
