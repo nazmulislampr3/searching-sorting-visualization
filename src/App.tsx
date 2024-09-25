1cc426be08ad0ca34d0b315a3d0a2c5f8a03338b
Md Nazmul Islam <nazmulislampr3@gmail.com>
2024-09-25 19:20:34 +0600

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Menu from "./components/Menu";
+import { useEffect } from "react";
 
 const App = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (pathname === "/") {
+      return navigate("/sorting/bubble");
+    }
+  }, [pathname]);
   return (
     <div className="h-screen overflow-hidden bg-blue-100 relative flex">
       <Menu />
